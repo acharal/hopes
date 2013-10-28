0a66c0a60967ce662ed32d7c9bd15c2628d2bd6a
Angelos Charalambidis <a.charalambidis@di.uoa.gr>
2013-10-28 23:01:40 +0200

    Move some functionality of driver to frontend module

--- src/interpreter/Driver.hs
+++ src/interpreter/Driver.hs
@@ -18,67 +18,22 @@
 -- | drives the pipeline of compilation + execution
 module Driver where
 
-
-import Language.Hopl (KnowledgeBase(..))
-
-import Parser (runParser, withInput, parseSrc, parseGoal, fromFile)
-import Tc (runTc, withSig, withTypeEnv)
-import WellForm (wfp, wfg)
-import Desugar (runDesugarT, desugarGoal, desugarSrc)
 import Infer
 import CoreLang (kbtoProgram, hopltoCoreGoal)
 import Pretty
 
-import Control.Monad.State (gets, modify)
-import Control.Monad.IO.Class (liftIO)
-import Control.Monad (when)
-
-import System.IO
-
+import Frontend
 import Debugger
 import HopesIO
 
-parseFromFile fname parser = do
-    file     <- liftIO $ openFile fname ReadMode
-    content  <- liftIO $ hGetContents file
-    parsed   <- runParser $ fromFile fname $ withInput content $ parser
-    case parsed of
-        Right (ast,s) -> return ast
-        Left   msgs -> processMsgs msgs
-
-processMsgs (errs, warns) = do
-    liftIO $ print $ vcat (map ppr errs)
-    fail "errors occured"
-
---loadSource :: String -> IO (Maybe (Prog, TypeEnv), Messages)
-
-loadSource file = do
-    parsed       <- liftIO $ parseFromFile file parseSrc
-    (wellformed, msgs) <- runTc (wfp parsed)
-    case wellformed of
-        Just (wfprog, env) -> do
-                cp <- runDesugarT $ desugarSrc (wfprog, env)
-                return (cp, env)
-        Nothing -> processMsgs msgs
-
-loadGoal inp env = do
-    parsed_res  <- liftIO $ runParser $ withInput inp $ parseGoal
-    parsed_goal <- case parsed_res of
-                        Right (g,_) -> return g
-                        Left msgs   -> processMsgs msgs
-    (tcres, msgs) <- runTc $ withSig parsed_goal $
-                              withTypeEnv env $ wfg parsed_goal
-    case tcres of
-        Just (tcgoal, env') -> do
-            cg <- runDesugarT $ desugarGoal (tcgoal, env')
-            return (hopltoCoreGoal cg)
-        Nothing -> processMsgs msgs
-
+import Control.Monad.State (gets, modify)
+import Control.Monad.IO.Class (liftIO)
+import Control.Monad (when)
+import System.IO (stdin, hSetBuffering, BufferMode(..))
 
 consultFile f = do
     (src, env) <- loadSource f
-    modify (\s -> s{ kb = KB src, 
-                     p = (kbtoProgram (KB src)), 
+    modify (\s -> s{ p = src, 
                      currentEnv = env})
     liftIO $ putStrLn ("% consulted " ++ show f ++ "")
 
@@ -86,22 +41,22 @@ sayYes    = putStrLn "Yes"
 sayNo     = putStrLn "No"
 
 refute s = do
-    src  <- gets kb
     env  <- gets currentEnv
     goal <- loadGoal s env
     consumeSolutions $ attachDebugger $ prove goal
+
 -- consumeSolutions :: Infer a b -> HopesIO ()
 consumeSolutions i = do
-    src  <- gets p
-    liftIO $ hSetBuffering stdin NoBuffering
+    src    <- gets p
     result <- infer src i
---    case runIdentity (infer src i) of
+
     case result of
         Nothing -> 
             liftIO $ sayNo
         Just (a, rest) -> do
             liftIO $ sayYes
             liftIO $ print $ ppr a
+            liftIO $ hSetBuffering stdin NoBuffering
             c <- liftIO $ getChar
             when (c == ';') $ do
                 liftIO $ putChar '\n';    
